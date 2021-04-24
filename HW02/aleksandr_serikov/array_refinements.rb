44bebc2ab77f594977993d2eb3b6a71b84577d03
Nij4life <nij4life@gmail.com>
2021-04-24 17:08:20 +0300

    feat(array_refinements): refine map

--- HW02/aleksandr_serikov/array_refinements.rb
+++ HW02/aleksandr_serikov/array_refinements.rb
@@ -1,18 +1,26 @@
 require 'pry'
 
 module ArrayRefinement
+
   refine Array do
+  
     def each
       for i in 0...size
         yield self[i] 
       end
       self
     end
+    
+    def map
+    new_array = []
+    for i in 0...size
+      new_array << (self[i] && yield(self[i]))
+    end
+    
+    new_array
+    end
+    
   end 
+  
 end
 
-using ArrayRefinement
-
-a = [1,2,3,4]
-pp a.each { |e| puts e }
- 
