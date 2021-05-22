eb3eb2c42d73d9472a37f040a6c81a7e2bbc9960
Nij4life <nij4life@gmail.com>
2021-05-22 17:19:46 +0300

    feat: add task1 tests

---
+++ HW03/aleksandr_serikov/spec/a_serikov_hw_03_t_01_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../a_serikov_hw_03_t_01'
+
+describe 'task1' do
+  context 'we have an error line' do
+    let(:logs) do
+      <<~LOGS
+        10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+        10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+        2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+        10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
+      LOGS
+    end
+
+    it 'returns string with error' do
+      result = task1(logs)
+      expect(result).to eql('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>')
+    end
+  end
+
+  context "we don't have a line with an error" do
+    let(:logs) do
+      <<~LOGS
+        10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+        10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+        10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
+      LOGS
+    end
+
+    it 'returns empty string' do
+      result = task1(logs)
+      expect(result).to eql('')
+    end
+  end
+end
